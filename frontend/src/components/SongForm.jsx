d14fb1d072ba74d61612ec02282bc011caff3787
Your Name <you@example.com>
2026-05-29 10:26:47 -0400

    feat: implementar formulario para agregar canciones

---
+++ frontend/src/components/SongForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+function SongForm({onAddSong}){
+    //Objeto
+    const [formData, setFormData] = useState({ //almacenamos estado 
+        title:'',
+        artist:'',
+        youtube_url:'',
+    });
+    //Cambios
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData, //copia del estado actual
+            [e.target.name]: e.target.value, //actualiza dinamicamente, interpretando input usuario 
+        });
+    }
+    //al enviar
+    const handleSubmit = async(e)=>{
+        e.preventDefault(); //Evita que la página web se recargue
+
+        await onAddSong(formData); // Se envía el objeto completo al componente padre; el código espera a que termine
+
+    setFormData({ //limpia el estado
+     title: '',
+     artist: '',
+     youtube_url: '',   
+    });
+    };
+
+ return (
+    <form onSubmit={handleSubmit} className="song-form">
+      <input
+        type="text"
+        name="title"
+        placeholder="Título"
+        value={formData.title}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="artist"
+        placeholder="Artista"
+        value={formData.artist}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="youtube_url"
+        placeholder="URL YouTube"
+        value={formData.youtube_url}
+        onChange={handleChange}
+      />
+
+      <button type="submit">
+        Agregar canción
+      </button>
+    </form>
+  );
+}
+
+export default SongForm;
--- frontend/src/services/songsApi.js
+++ frontend/src/services/songsApi.js
@@ -1,6 +1,6 @@
 //Set URL Songs
 const API_URL = 'http://localhost:3000/api/songs';
-
+//pide datos al backend 
 export const getSongs = async () => {
   try {
     const response = await fetch(API_URL);
@@ -16,3 +16,22 @@ export const getSongs = async () => {
     return [];
   }
 };
+//envía datos al backend usando POST
+export const createSong = async(songData) =>{
+  try {
+    const response = await fetch(API_URL,{
+    method: 'POST',
+    headers: {
+      'content-type':'application/json',
+    },
+    body: JSON.stringify(songData),
+
+    });
+    if (!response.ok) {
+      throw new Error('Error al crear canción');
+    }
+      return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
